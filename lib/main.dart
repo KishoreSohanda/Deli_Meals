16ee4048ba37ee0e6f4329e1550adb9c1375e985
Kishore <kishoresohandacs19@acropolis.in>
2023-03-20 16:37:56 +0530

    Started my new project DeliMeals and in this project added categories and categoriesItem routes and also added some theme Style.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyLarge: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyMedium: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleSmall: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
